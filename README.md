5db80e6519eeea292ae75816c9295cb61575f669
bingleyu <bingle.yu@gmail.com>
2013-06-02 11:28:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+== Welcome to Rails
+
+# Ruby on Rails Tutorial: demo app
+
+This is the demo app for
+[*Ruby on Rails Tutorial : Learning Rails by example*]
+(http://railstutorial.org/) by [Michael Hartl](http://michaelhartl.com/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '4aa0ed1ef0d1a2fc85f1a6c48cec7d15ad57a5ca965fea7e4a4d84a321223e4999472553f8c8e1a4fb166f5761d1762c4a7bf42dadac70366fa6109d0e4aa73b'
